4a59b217fd0902d322f8ea509678a9790882b123
vrtx <me@vrtx.sh>
2026-04-23 10:32:34 +0100

    generate wireguard config using yq, introduce preshared keys

--- sophons/gen.sh
+++ sophons/gen.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 set -xeuo pipefail
 
@@ -19,6 +19,8 @@ gen_controlplane() {
   local host_name=$1
   local image=$2
 
+  gen_wg $host_name "$patch_dir/wg.kluster.sops.yaml"
+
   talosctl gen config $cluster $endpoint \
     --force --with-examples=false \
     --install-image=$image \
@@ -29,6 +31,7 @@ gen_controlplane() {
     --config-patch "@$patch_dir/chrome.seccomp.yaml" \
     --config-patch "@$patch_dir/controlplane.sops.yaml" \
     --config-patch "@$patch_dir/$host_name.sops.yaml" \
+    --config-patch "@$self_dir/generated/wg.kluster.$host_name.yaml" \
     --output-types controlplane \
     --output "$self_dir/generated/controlplane.$host_name.yaml"
 
@@ -42,6 +45,8 @@ gen_worker() {
   local host_name=$1
   local image=$2
 
+  gen_wg $host_name "$patch_dir/wg.kluster.sops.yaml"
+
   talosctl gen config $cluster $endpoint \
     --force --with-examples=false \
     --install-image=$image \
@@ -51,12 +56,60 @@ gen_worker() {
     --config-patch "@$patch_dir/common.sops.yaml" \
     --config-patch "@$patch_dir/chrome.seccomp.yaml" \
     --config-patch "@$patch_dir/$host_name.sops.yaml" \
+    --config-patch "@$self_dir/generated/wg.kluster.$host_name.yaml" \
     --output-types worker \
     --output "$self_dir/generated/worker.$host_name.yaml"
 }
 
+gen_wg() {
+  local host_name="$1"
+  local config="$2"
+  local interface="$(basename "$config" | sed s/.sops.yaml$//)"
+  local enriched="$self_dir/generated/$interface.yaml"
+
+  local name
+  cp "$config" "$enriched"
+  for name in $(yq '.peers | keys | .[]' "$enriched"); do
+    pubkey=$(yq ".wireguard.privateKeys.$name" "$enriched" | wg pubkey)
+    yq --inplace ".peers.$name.publicKey = \"$pubkey\"" $enriched
+  done
+
+  HOST_NAME="$host_name" INTERFACE="$interface" yq '
+    env(HOST_NAME) as $selfName |
+    .peers[$selfName] as $self  |
+    .wireguard as $wg |
+    [.peers | to_entries[] | select(.key != $selfName)] as $peers |
+    {
+      "apiVersion": "v1alpha1",
+      "kind": "WireguardConfig",
+      "name": env(INTERFACE),
+      "up": true,
+      "privateKey": $wg.privateKeys[$selfName],
+      "listenPort": $wg.listenPort,
+      "addresses": [$self.addresses[] | {"address": .}],
+      "peers": [
+        $peers[] | .key as $peerName | .value as $peer |
+        ([$selfName, $peerName] | sort | join(":")) as $pairKey |
+        {
+          "publicKey": $peer.publicKey,
+          "presharedKey": $wg.presharedKeys[$pairKey],
+          "allowedIPs": ($peer.addresses + $peer.allowedIPs)
+        } |
+        # skip endpoint only when peer is behind a NAT and we arent
+        with(select($peer.nat == false or $self.nat == true);
+          .endpoint = (($peer.endpoints[($self.via // "v4")] // $peer.endpoints.v4) + ":" + ($wg.listenPort | tostring))
+        ) |
+        with(select(($self.persistentKeepaliveInterval // "") != "");
+          .persistentKeepaliveInterval = $self.persistentKeepaliveInterval
+        )
+      ],
+      "routes": [$peers[].value | (.addresses + .allowedIPs + (.routes // []))[] | {"destination": .}]
+    }
+  ' "$enriched" >"$self_dir/generated/$interface.$host_name.yaml"
+}
+
+rm -r $self_dir/generated
 mkdir -p $self_dir/generated
-rm -rf $self_dir/generated/*.yaml
 
 gen_controlplane beta factory.talos.dev/installer/$pi_image:$talos_version
 gen_controlplane theta factory.talos.dev/installer/$vm_image:$talos_version
